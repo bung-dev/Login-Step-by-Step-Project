fc8e60d761added74c98cc3a3947f011e4c2ca94
bung-dev <wndgh26@gmail.com>
2026-01-06 17:47:41 +0900

    ✅ test: add repository refactor and service tests

--- src/test/java/project/member/MemberRepositoryTest.java
+++ src/test/java/project/member/MemberRepositoryTest.java
@@ -2,6 +2,9 @@ package project.member;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
@@ -20,9 +23,13 @@ class MemberRepositoryTest {
     @Test
     void saveAndFindById(){
         //given
-        MemberRequest memberRequest = new MemberRequest("test1","test!","test1");
+        Member member1 = Member.builder()
+                .loginId("test1")
+                .password("test!")
+                .name("test1")
+                .build();
         //when
-        Member member = memberRepository.save(memberRequest);
+        Member member = memberRepository.save(member1);
         //then
         Member findMember = memberRepository.findById(member.getId());
         assertThat(findMember).isEqualTo(member);
@@ -31,26 +38,38 @@ class MemberRepositoryTest {
     @Test
     void findAll(){
         //given
-        MemberRequest memberRequest = new MemberRequest("test1","test!","test1");
-        MemberRequest memberRequestTwo = new MemberRequest("test2","test!","test2");
+        Member member1 = Member.builder()
+                .loginId("test1")
+                .password("test!")
+                .name("test1")
+                .build();
+        Member member2 = Member.builder()
+                .loginId("test2")
+                .password("test!")
+                .name("test2")
+                .build();
 
-        memberRepository.save(memberRequest);
-        memberRepository.save(memberRequestTwo);
+        memberRepository.save(member1);
+        memberRepository.save(member2);
         //when
         List<Member> all = memberRepository.findAll();
 
         //then
         assertThat(all.size()).isEqualTo(2);
-        assertThat(all.get(0).getName()).isEqualTo(memberRequest.name());
-        assertThat(all.get(1).getName()).isEqualTo(memberRequestTwo.name());
+        assertThat(all.get(0).getName()).isEqualTo(member1.getName());
+        assertThat(all.get(1).getName()).isEqualTo(member2.getName());
     }
 
     @Test
     void deleteByMemberId(){
         //given
-        MemberRequest memberRequest = new MemberRequest("test1","test!","test1");
+        Member member1 = Member.builder()
+                .loginId("test1")
+                .password("test!")
+                .name("test1")
+                .build();
 
-        Member member = memberRepository.save(memberRequest);
+        Member member = memberRepository.save(member1);
         //when
         System.out.println(member.toString());
         memberRepository.deleteMemberById(member.getId());
---
+++ src/test/java/project/member/MembersServiceTest.java
@@ -0,0 +1,60 @@
+package project.member;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class MembersServiceTest {
+
+    MemberRepository memberRepository = new MemberRepository();
+
+    MemberService memberService = new MemberService(memberRepository);
+
+    @Test
+    void createMemberAndGetMemberById(){
+        // given
+        MemberRequest memberRequest = new MemberRequest("test1", "test!", "test1");
+        // when
+        MemberResponse memberResponse = memberService.createMember(memberRequest);
+        MemberResponse getMemberByIdResponse = memberService.getMemberById(1L);
+        // then
+        assertThat(memberResponse.name()).isEqualTo(getMemberByIdResponse.name());
+
+    }
+
+    @Test
+    void getAllMembers(){
+        //given
+        MemberRequest memberRequest1 = new MemberRequest("test1", "test!", "test1");
+        MemberRequest memberRequest2 = new MemberRequest("test2", "test!", "test2");
+
+        MemberResponse member1 = memberService.createMember(memberRequest1);
+        MemberResponse member2 = memberService.createMember(memberRequest2);
+
+        //when
+        List<MemberResponse> allMembers = memberService.getAllMembers();
+
+        //then
+        assertThat(allMembers.size()).isEqualTo(2);
+        assertThat(allMembers.get(0).name()).isEqualTo(member1.name());
+        assertThat(allMembers.get(1).name()).isEqualTo(member2.name());
+    }
+
+    @Test
+    void deleteByMember(){
+        //given
+        MemberRequest memberRequest = new MemberRequest("test1", "test!", "test1");
+
+        memberService.createMember(memberRequest);
+        //when
+        memberService.deleteMember(1L);
+        //then
+        assertThat(memberService.getMemberById(1L)).isNull();
+    }
+}
