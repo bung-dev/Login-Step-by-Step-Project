c33473f624e46c73fb3f3d5b5ef81ec2b567e243
bung-dev <wndgh26@gmail.com>
2026-01-06 18:32:51 +0900

    ♻️ refactor: remove imports

--- src/test/java/project/member/MemberRepositoryTest.java
+++ src/test/java/project/member/MemberRepositoryTest.java
@@ -2,9 +2,6 @@ package project.member;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
--- src/test/java/project/member/MembersServiceTest.java
+++ src/test/java/project/member/MembersServiceTest.java
@@ -1,10 +1,6 @@
 package project.member;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
