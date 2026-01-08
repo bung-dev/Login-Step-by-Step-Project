fb0f9b72be09ce2547e3a991a078dbd4c752d153
bung-dev <wndgh26@gmail.com>
2026-01-08 20:47:01 +0900

    ✨ feat: add login check interceptor

---
+++ src/main/java/project/member/web/interceptor/LoginCheckInterceptor.java
@@ -0,0 +1,29 @@
+package project.member.web.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.extern.log4j.Log4j2;
+import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import static project.member.web.SessionConst.LOGIN_MEMBER;
+
+@Log4j2
+public class LoginCheckInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(@NonNull HttpServletRequest request,
+                             @NonNull HttpServletResponse response,
+                             @NonNull Object handler) throws Exception {
+        HttpSession session = request.getSession();
+
+        if (session == null || session.getAttribute(LOGIN_MEMBER) == null) {
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+
+            return false;
+        }
+        return true;
+    }
+}
