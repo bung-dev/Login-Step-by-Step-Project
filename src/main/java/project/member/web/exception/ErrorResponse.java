1714ebdeeb2e533153f7baa3208758eebbf71608
bung-dev <wndgh26@gmail.com>
2026-01-10 19:01:17 +0900

    ✨ feat: enhance ErrorResponse with message and validation errors

--- src/main/java/project/member/web/exception/ErrorResponse.java
+++ src/main/java/project/member/web/exception/ErrorResponse.java
@@ -1,7 +1,13 @@
 package project.member.web.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.util.Map;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public record ErrorResponse(
         String errorCode,
-        String massage
+        String message,
+        Map<String, String> errors
 ) {
 }
