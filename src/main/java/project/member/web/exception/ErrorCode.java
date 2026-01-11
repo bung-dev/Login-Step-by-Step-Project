f2418022e514fae92735252a66ec0451ce0609a1
bung-dev <wndgh26@gmail.com>
2026-01-11 17:21:10 +0900

    ✨ feat: add error code for invalid login credentials

--- src/main/java/project/member/web/exception/ErrorCode.java
+++ src/main/java/project/member/web/exception/ErrorCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEMBER_404","존재하지 않는 회원입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "MEMBER_401", "인증되지 않은 요청입니다"),
+    INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "MEMBER_401_INVALID", "아이디 또는 비밀번호가 올바르지 않습니다."),
 
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다.");
 
