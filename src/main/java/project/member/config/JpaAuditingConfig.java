6b1180877979dad5467eaee1933dbc1b63ae8187
bung-dev <wndgh26@gmail.com>
2026-01-14 23:42:21 +0900

    ✨ feat: enable JPA auditing configuration

---
+++ src/main/java/project/member/config/JpaAuditingConfig.java
@@ -0,0 +1,10 @@
+package project.member.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
