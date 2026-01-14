1e4e6beb75a1108456e1d8e58f738dd81e75f26e
bung-dev <wndgh26@gmail.com>
2026-01-14 23:40:04 +0900

    ♻️ refactor: replace member Controller

--- src/main/java/project/member/web/exception/ErrorCode.java
+++ src/main/java/project/member/web/exception/ErrorCode.java
@@ -10,6 +10,7 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND,"MEMBER_404","존재하지 않는 회원입니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "MEMBER_401", "인증되지 않은 요청입니다"),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "MEMBER_401_INVALID", "아이디 또는 비밀번호가 올바르지 않습니다."),
+    DUPLICATE_LOGIN_ID(HttpStatus.CONFLICT, "MEMBER_409", "서버가 요청을 처리할 수 없습니다"),
 
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "COMMON_400", "잘못된 요청입니다.");
 
@@ -17,4 +18,8 @@ public enum ErrorCode {
     private final HttpStatus status;
     private final String code;
     private final String message;
+
+    public CustomException exception() {
+        return new CustomException(this);
+    }
 }
