0a0c0e00a790681bec74c3208b1736d1b06de7d1
bung-dev <wndgh26@gmail.com>
2026-01-17 12:15:31 +0900

    ✨ feat: add CustomUserDetailsService for authentication

---
+++ src/main/java/project/member/security/CustomMemberDetailsService.java
@@ -0,0 +1,23 @@
+package project.member.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import project.member.domain.Member;
+import project.member.repository.MemberRepository;
+
+@Service
+@RequiredArgsConstructor
+public class CustomMemberDetailsService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member member = memberRepository.findByLoginId(username).orElseThrow(()
+                -> new UsernameNotFoundException("사용자를 찾을 수 없습니다" + username));
+        return new CustomMemberDetails(member);
+    }
+}
