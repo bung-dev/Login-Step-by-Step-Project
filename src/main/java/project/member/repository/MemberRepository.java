254fdeb10c5e5f969f5239179069e4d3008e240b
bung-dev <wndgh26@gmail.com>
2026-01-19 22:12:15 +0900

    ✨ feat: add findAllByDeletedAtIsNull

--- src/main/java/project/member/repository/MemberRepository.java
+++ src/main/java/project/member/repository/MemberRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.member.domain.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -14,4 +15,6 @@ public interface MemberRepository extends JpaRepository<Member,Long> {
     Optional<Member> findByIdAndDeletedAtIsNull(Long id);
 
     boolean existsByLoginId(String loginId);
+
+    List<Member> findAllByDeletedAtIsNull();
 }
