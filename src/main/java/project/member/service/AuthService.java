cea1227110522addbcf488086e0b93a029afae3c
bung-dev <wndgh26@gmail.com>
2026-01-23 11:54:31 +0900

    ✨ feat: generate and persist refresh tokens in AuthService

--- src/main/java/project/member/service/AuthService.java
+++ src/main/java/project/member/service/AuthService.java
@@ -1,14 +1,16 @@
 package project.member.service;
 
+import jakarta.servlet.http.Cookie;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
-import project.member.domain.Role;
+import project.member.domain.Refresh;
 import project.member.domain.dto.LoginRequest;
 import project.member.domain.dto.TokenResponse;
+import project.member.repository.RefreshRepository;
 import project.member.security.jwt.JWTUtil;
 import project.member.web.exception.ErrorCode;
 
@@ -20,6 +22,7 @@ public class AuthService {
 
     private final AuthenticationManager authenticationManager;
     private final JWTUtil jwtUtil;
+    private final RefreshRepository refreshRepository;
 
     public TokenResponse login(LoginRequest request) {
         try{
@@ -33,11 +36,19 @@ public class AuthService {
                     .getAuthority();
 
             String accessToken = jwtUtil.createToken(loginId, role, JWT_ACCESS_TOKEN_NAME , JWT_ACCESS_TOKEN_EXPIRED_TIME);
+            String refreshToken = jwtUtil.createToken(loginId, role, JWT_REFRESH_TOKEN_NAME, JWT_REFRESH_TOKEN_EXPIRED_TIME);
+            addRefreshToken(loginId,refreshToken);
 
-            return TokenResponse.from(accessToken, JWT_ACCESS_TOKEN_NAME);
+            return TokenResponse.from(accessToken, refreshToken);
 
         } catch (BadCredentialsException e) {
             throw ErrorCode.INVALID_CREDENTIALS.exception();
         }
+    }
+
+    protected void addRefreshToken(String loginId, String refreshToken) {
+        long expiresAtMillis = System.currentTimeMillis() + JWT_REFRESH_TOKEN_EXPIRED_TIME;
+        Refresh refresh = Refresh.create(loginId, refreshToken, expiresAtMillis);
+        refreshRepository.save(refresh);
     }
 }
