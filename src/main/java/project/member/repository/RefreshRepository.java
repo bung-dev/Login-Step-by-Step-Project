6202922bb5212e4084a9283901c1d0bb6944b397
bung-dev <wndgh26@gmail.com>
2026-01-26 14:38:57 +0900

    ✨ feat: add findByLoginId

--- src/main/java/project/member/repository/RefreshRepository.java
+++ src/main/java/project/member/repository/RefreshRepository.java
@@ -4,11 +4,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import project.member.domain.Refresh;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RefreshRepository extends JpaRepository<Refresh, Long> {
     Optional<Refresh> findByRefresh(String refresh);
+
+    List<Refresh> findByLoginId(String loginId);
 
     void deleteByRefresh(String refresh);
 
